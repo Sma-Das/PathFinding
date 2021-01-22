2f4654629ccb7332b678071bb83fedd5f4afe4c2
Sma-Das <smaddas10@gmail.com>
2021-01-22 11:35:26 +0400

    Removed unneeded comments

--- ScanImage.py
+++ ScanImage.py
@@ -4,28 +4,9 @@ from queue import PriorityQueue
 from dataclasses import dataclass
 
 
-# maze = cv2.imread("Maze_Pictures/tiny.png", 0)
-# rows, columns = maze.shape[:2]
-# EMPTY, WALL = 255, 0
-
-
 def neighbors(row: int, column: int, maze: np.ndarray) -> tuple[tuple[int]]:
     yield maze[row+1][column], maze[row-1][column]
     yield maze[row][column+1],  maze[row][column-1]
-
-
-# def find_nodes(maze: np.ndarray):
-#     open_set = PriorityQueue()
-#     open_set.put((0, 0, maze[0].tolist().index(EMPTY)))
-#     count = 1
-#     for row in range(1, rows-1):
-#         for column in range(1, columns-1):
-#             (up, down), (left, right) = neighbors(row, column, maze)
-#             if (up or down) and (left or right) and maze[row][column] == EMPTY:
-#                 open_set.put((count, row, column))
-#                 count += 1
-#     open_set.put((count, rows-1, maze[rows-1].tolist().index(EMPTY)))  # End
-#     return open_set
 
 
 @dataclass
