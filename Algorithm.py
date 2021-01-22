083482e59c7b3bc8c9ab3a8b0dbfd3d640e8b5b0
Sma-Das <smaddas10@gmail.com>
2021-01-22 13:55:42 +0400

    Initial commit

--- Algorithm.py
+++ Algorithm.py
@@ -1,2 +1,23 @@
-# from ScanImage import find_nodes
 from ScanImage import FindNodes
+
+
+def fScore(start, curr):
+    ''' score from the start to curr node '''
+    s_row, s_col = start
+    c_row, c_col = curr
+    return (c_row - s_row) + (c_col - s_col)
+
+
+def gScore(end, curr):
+    ''' score from end to curr node '''
+    e_row, e_col = end
+    c_row, c_col = curr
+    return (c_row - e_row) + (c_col - e_col)
+
+
+if __name__ == '__main__':
+
+    # maze = sys.argv[1]
+    solver = FindNodes("Maze_Pictures/tiny.png")
+    nodes = solver.find_nodes()
+    print(nodes.qsize())
